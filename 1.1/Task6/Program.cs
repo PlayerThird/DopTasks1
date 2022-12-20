0ab5814d94be37e0b8683ef1438d2b01a6dd42e2
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 21:32:40 +0300

    Task 6

---
+++ 1.1/Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число от 0 до 5");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0 || num > 5){
+    Console.WriteLine("Я тебе что сказал!? >:(");
+    return;
+}
+switch (num)
+{
+    case 0:
+    Console.WriteLine("Zero");
+    break;
+    case 1:
+    Console.WriteLine("One");
+    break;
+    case 2:
+    Console.WriteLine("Second");
+    break;
+    case 3:
+    Console.WriteLine("Third");
+    break;
+    case 4:
+    Console.WriteLine("Four");
+    break;
+    case 5:
+    Console.WriteLine("Five");
+    break;
+}
