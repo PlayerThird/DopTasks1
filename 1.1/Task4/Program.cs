ebad9a2963f98e30a749376ef56373207ad0d0a3
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 20:39:48 +0300

    Task 4

---
+++ 1.1/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер месяца");
+int mes = Convert.ToInt32(Console.ReadLine());
+if (mes > 12 || mes < 1)
+{
+    Console.WriteLine("Неа, дружище, давай еще раз");
+    return;
+}
+if (mes <= 2 || mes == 12)
+{
+    Console.WriteLine("Ну да, зимой у нас сессия");
+}
+else if(mes <= 5)
+{
+    Console.WriteLine("Ну да, каникулы весной, а что такого?");
+}
+else if(mes <= 8)
+{
+    Console.WriteLine("Ну да, летом начинается 1 семестр");
+}
+else if(mes <= 11)
+{
+    Console.WriteLine("Ну да, страдай дальше в 2м семестре");
+}
