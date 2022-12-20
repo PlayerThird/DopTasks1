0594e6f0594db6b87c76463f0700378eec87b7ce
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 22:00:15 +0300

    Task8

---
+++ 1.1/Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите букву предмета");
+string buk = Console.ReadLine();
+int lem = buk.Length;
+if (lem > 1)
+{
+    Console.WriteLine("Буква = 1 буква, давай еще раз");
+    return;
+}
+switch (buk)
+{
+    case "б":
+    Console.WriteLine("Белорусь, Минск");
+    break;
+    case "р":
+    Console.WriteLine("Россия, МКАД");
+    break;
+    case "л":
+    Console.WriteLine("Литва, Вильнос");
+    break;
+    default: Console.WriteLine("Ну, видимо еще не добавили такого города или он горит в ядерном пепле --_--");
+    break;
+}
