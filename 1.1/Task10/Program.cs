cc88b64363c065b6dc5ae9e4bfa011df88108a8a
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 23:42:47 +0300

    Task 10 с эксперементальной папкой(пароль не удалось нормально проверить(консоль сама отлично справляется))

---
+++ 1.1/Task10/Program.cs
@@ -0,0 +1,36 @@
+﻿//Проверяем логин (только буквы, 5 символов) и пароль (только цифры, 6 цифр)
+int Pas1 = 0;
+Console.WriteLine("Введите логин(5 символов)");
+string Log = Console.ReadLine();
+
+foreach (var latter in Log)
+{
+    if (Char.IsDigit(latter) == true) //Проверка, true это наличие цифры
+    {
+        Console.WriteLine("Воу, воу, тут цифра");
+        return;
+    }
+}
+if (Log.Length > 0 && Log.Length < 6)
+{
+    Console.WriteLine("Всё норм");
+}
+else{
+    Console.WriteLine("Эй-эй, тут логин не более 5 символов, гадай еще");
+    return;
+}
+
+Console.WriteLine("Введите пороль(6 цифр)");
+string Pas = Console.ReadLine();
+Pas1 = int.Parse(Pas);
+
+if (int.TryParse(Pas, out Pas1) || Pas.Length < 7 && Pas.Length > 0)
+{
+    Console.WriteLine("Всё норм");
+}
+else{
+    Console.WriteLine("Эй-эй, тут пароль более 6 символов, гадай еще");
+    return;
+}
+
+Console.WriteLine("Ну, здарова мир)");
---
+++ 1.1/Task10Experemental/Program.cs
@@ -0,0 +1,11 @@
+﻿string word = "Длинное44слово";
+Console.WriteLine(Char.IsDigit(word));
+foreach (var latter in word)
+{
+    Console.WriteLine(Char.IsDigit(latter));
+    if (Char.IsDigit(latter) == true)
+    {
+        Console.WriteLine("Воу, воу, тут цифра");
+        return;
+    }
+}
