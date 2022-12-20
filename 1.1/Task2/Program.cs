ffe013dad9a56de807ba58aa668e84e6039d03ba
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 20:19:41 +0300

    Task 2

---
+++ 1.1/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int first = new Random().Next(10, 30);
+int second = new Random().Next(10, 30);
+Console.WriteLine($"First " + (first) + " Second " + (second));
+if (first > second)
+{
+    Console.WriteLine(first - second);
+}
+else if(first < second){
+    Console.WriteLine(first + second);
+}
+else
+{
+    Console.WriteLine("А числа то равны!");
+}
