daa4a888b90164f0d151d073da4a2522fd8cf643
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 20:31:03 +0300

    Task 3

---
+++ 1.1/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int c = new Random().Next(-10, 10);
+int d = new Random().Next(-10,10);
+Console.WriteLine("First " + (c) + " Second " + (d));
+if (c % 2 == 0 && d % 2 == 0)
+{
+    Console.WriteLine("Деление "+(c/d));
+}
+else if(c % 2 != 0 && d % 2 != 0){
+    Console.WriteLine("Сумма "+(c + d));
+}
+else
+{
+    Console.WriteLine("Умножение "+(c * d));
+}
