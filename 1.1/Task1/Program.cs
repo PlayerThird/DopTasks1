485d6a9d11a0da9897241e0828b8ecf474c03f1c
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 20:12:34 +0300

    Initial git and sozdatj task 1

---
+++ 1.1/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = new Random().Next(20, 90);
+Console.WriteLine(n);
+Console.WriteLine("Введите делимое число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (n%x ==0){
+ Console.WriteLine("Да, оно делится");}
+ else
+ {
+    Console.WriteLine("Nope, оно не делится");
+ }
+
