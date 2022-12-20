17365c5fc97b986963a25f9b2c41116c4bbbadb7
PlayerThird030 <verevka.rud@gmail.com>
2022-12-20 21:56:01 +0300

    Task7

---
+++ 1.1/Task7/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите букву предмета");
+string buk = Console.ReadLine();
+int lem = buk.Length;
+if (lem > 1)
+{
+    Console.WriteLine("Буква = 1 буква, давай еще раз");
+    return;
+}
+switch (buk)
+{
+    case "ф":
+    Console.WriteLine("Физика");
+    break;
+    case "и":
+    Console.WriteLine("История");
+    break;
+    case "г":
+    Console.WriteLine("География");
+    break;
+    case "б":
+    Console.WriteLine("Биология");
+    break;
+    case "м":
+    Console.WriteLine("Математика");
+    break;
+    case "р":
+    Console.WriteLine("Русский язык");
+    break;
+    case "л":
+    Console.WriteLine("Литература");
+    break;
+    case "х":
+    Console.WriteLine("Химия");
+    break;
+    default: Console.WriteLine("Ну, видимо еще не добавили такого элемента");
+    break;
+}
