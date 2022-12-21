ec78ea4b4512866f55db7060bfe3d247d7684d02
PlayerThird030 <verevka.rud@gmail.com>
2022-12-21 23:42:31 +0300

    Task 5

---
+++ 1.2/Task5/Program.cs
@@ -0,0 +1,7 @@
+﻿    do
+ {
+    Console.WriteLine("Введите радиус: ");
+    float r = float.Parse(Console.ReadLine());
+    Console.WriteLine("Радиус равен: " + (3.14*r*r));
+    Console.WriteLine("Введите Z для выхода если нет, то любой другой символ: ");
+ } while (Console.ReadKey(true).Key != ConsoleKey.Z);
