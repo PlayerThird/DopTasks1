c828fd43ad47f876ee0b399bc6e7c55f8837ea0f
PlayerThird030 <verevka.rud@gmail.com>
2022-12-21 22:09:46 +0300

    Task 1

--- 1.2/Task1/Program.cs
+++ 1.2/Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первую цифру: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую цифру: ");
+int n2 = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = n1; i < n2+1; i++){
+    if (i%2 != 0)
+    {
+        sum += i;
+    }
+    Console.Write("I = "+ i);
+    Console.WriteLine("Sum= " + sum);
+}
+Console.WriteLine(sum);
