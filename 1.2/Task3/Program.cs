7a9f27da4804f7ac3637d56c211d803f5005f9f2
PlayerThird030 <verevka.rud@gmail.com>
2022-12-21 23:14:52 +0300

    Task 3

---
+++ 1.2/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вычисляется и выводится в таблицу с шагом 10км стоимость поездки
+//на автомобиле в зависимости от растояни S(10-90км).
+//С клавиатуры вводятся: b - расход бензина на 100км
+//с - цена бензина за 1 литр
+Console.WriteLine("Ведите растояние: ");
+int S = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите расход бензина: ");
+float ras = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите цену на ваш бензин: ");
+float k = float.Parse(Console.ReadLine());
+float itogo = 0;
+for (int i = 10; i < S+10; i+=10)
+{
+    Console.WriteLine("Расстояние = " + i);
+    itogo += ras/10 * k;
+    Console.WriteLine("Цена на бенз = " + itogo);
+}
