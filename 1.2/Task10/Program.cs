d96be7c0d8fc43d58921cd9afcafa926b10b07e3
PlayerThird030 <verevka.rud@gmail.com>
2022-12-22 22:52:10 +0300

    Task 10

---
+++ 1.2/Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Ежедневно количество бактерий увеличивается на р процентов
+//Через сколько дней количество бактерий увеличится в n раз (ввод р и n)
+void Bacterial(double x, double y, double sum){
+    double proc1 = y / 100;
+    double itogo = sum*x;
+    for (int i = 0; sum < itogo; i++)
+        {
+            if (i > 100)
+            {
+                break;
+            }
+            sum += sum*proc1;
+            Console.WriteLine("День {0}: сумма бактерий = {1:f2}", i, sum);
+        }
+}
+Console.Write("Введите кол-во бактерий = ");
+double bac = double.Parse(Console.ReadLine());
+Console.Write("Введите процент на который увеличивается кол-во бактерий в день = ");
+double proc = double.Parse(Console.ReadLine());
+Console.Write("Кол-во бактерий увеличивается в n раз? n = ");
+double n = double.Parse(Console.ReadLine());
+Bacterial(n, proc, bac);
