b748d6f6de7c65884cd8d7b0e0353a0dc73cbfcc
PlayerThird030 <verevka.rud@gmail.com>
2022-12-22 09:10:03 +0300

    Task 7

---
+++ 1.2/Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 0; i < 10; i++)
+         {
+            int x = new Random().Next(-30, 30);
+            Console.WriteLine(x);
+            if (x > 0)
+            {
+                Console.Write(" положительное ");
+                if (x%2 != 0)
+            {
+                Console.WriteLine(" нечетное ");
+            }
+            else
+            {
+                Console.WriteLine(" чётное ");
+            }
+            }
+         }
